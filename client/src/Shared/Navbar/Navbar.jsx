bc4302fe6369074a6640317a97c401aef510d640
abrarasif11 <faasif929@gmail.com>
2025-10-27 22:10:24 +0600

    use clerk

--- client/src/Shared/Navbar/Navbar.jsx
+++ client/src/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { ArrowRight } from "lucide-react";
 import Logo from "../Logo/Logo";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
   return (
     <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
       <Logo className="cursor-pointer" />
-      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get Stared <ArrowRight className="w-4 h-4" />
-      </button>
+      {user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get Stared <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };
