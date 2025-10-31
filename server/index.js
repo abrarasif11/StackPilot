0acc79712e636fb2ae3f49a57b083a643c37b40a
abrarasif11 <faasif929@gmail.com>
2025-10-31 17:03:32 +0600

    server create

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+dotenv.config();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("StackPilot Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
